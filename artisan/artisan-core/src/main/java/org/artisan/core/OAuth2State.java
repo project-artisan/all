3a78572e339aa9b1f71e8a40c52a5fe2d7ad992f
JaehongDev <println@kakao.com>
2025-02-20 15:27:06 +0900

    feat: OAuth2 상태 추가

---
+++ artisan/artisan-core/src/main/java/org/artisan/core/OAuth2State.java
@@ -0,0 +1,12 @@
+package org.artisan.core;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum OAuth2State {
+    GITHUB("github"),
+
+    ;
+    private final String value;
+
+}
