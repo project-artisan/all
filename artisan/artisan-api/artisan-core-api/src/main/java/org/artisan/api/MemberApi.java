181fba55e3affa4588ad9cc73189b7ea2057336a
JaehongDev <println@kakao.com>
2025-02-20 22:19:06 +0900

    feat: 회원 api 재정의

---
+++ artisan/artisan-api/artisan-core-api/src/main/java/org/artisan/api/MemberApi.java
@@ -0,0 +1,20 @@
+package org.artisan.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.artisan.attributes.Auth;
+import org.artisan.attributes.MemberOnly;
+import org.artisan.core.User;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApi {
+
+
+    @MemberOnly
+    public MemberProfileResponse getMe(@Auth User user) {
+        return null;
+    }
+}
+
