a6e0698fb06318d7334fcef18a9f21f9124cd261
JaehongDev <println@kakao.com>
2025-02-20 17:16:07 +0900

    feat: 게시글 읽음 처리 리포지터릴 정의

---
+++ artisan/artisan-domain/artisan-drrr-domain/src/main/java/org/artisan/domain/PostReadHistoryRepository.java
@@ -0,0 +1,11 @@
+package org.artisan.domain;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostReadHistoryRepository extends JpaRepository<PostReadHistory, Long> {
+
+    Long countByMemberId(Long memberId);
+
+    List<PostReadHistory> findDistinctByMemberIdAndPostIdIn(Long memberId, List<Long> postIds);
+}
