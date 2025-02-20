9cb936c96f0b88157af44922891823bbe23d24b5
JaehongDev <println@kakao.com>
2025-02-20 17:02:36 +0900

    refactor: baseEntity getter 추가

--- artisan/artisan-domain/artisan-core-domain/src/main/java/org/artisan/core/domain/BaseEntity.java
+++ artisan/artisan-domain/artisan-core-domain/src/main/java/org/artisan/core/domain/BaseEntity.java
@@ -6,9 +6,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Getter
 @MappedSuperclass
 public class BaseEntity {
 
