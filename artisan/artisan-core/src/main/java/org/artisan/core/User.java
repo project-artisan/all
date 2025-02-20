d1000e33536c5de9599259cff22a7503fc06a6b0
JaehongDev <println@kakao.com>
2025-02-20 21:06:52 +0900

    refactor: User 정적 팩토리 메서드 추가

--- artisan/artisan-core/src/main/java/org/artisan/core/User.java
+++ artisan/artisan-core/src/main/java/org/artisan/core/User.java
@@ -4,4 +4,12 @@ public record User(Long id, Role role){
     public boolean isMember() {
         return role == Role.MEMBER;
     }
+
+    public static User member(Long id) {
+        return new User(id , Role.MEMBER);
+    }
+
+    public static User guest() {
+        return new User(null, Role.GUEST);
+    }
 }
