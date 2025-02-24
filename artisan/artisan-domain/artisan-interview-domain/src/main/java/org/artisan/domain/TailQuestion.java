3739cbc361f961fd84f58bc1cab5ea5aa3f40bb6
JaehongDev <println@kakao.com>
2025-02-24 15:27:06 +0900

    feat: TailQuestion 엔티티 정의

--- artisan/artisan-domain/artisan-interview-domain/src/main/java/org/artisan/domain/TailQuestion.java
+++ artisan/artisan-domain/artisan-interview-domain/src/main/java/org/artisan/domain/TailQuestion.java
@@ -1,6 +1,10 @@
 package org.artisan.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import org.artisan.core.domain.BaseEntity;
@@ -10,7 +14,26 @@ import org.artisan.core.domain.BaseEntity;
 public class TailQuestion extends BaseEntity {
 
 
-    @ManyToOne
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Interview interview;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private InterviewQuestion interviewQuestion;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Embedded
+    private AIFeedback aiFeedback;
+
+    @Embedded
+    private Answer answer = Answer.init();
+
 
 }
