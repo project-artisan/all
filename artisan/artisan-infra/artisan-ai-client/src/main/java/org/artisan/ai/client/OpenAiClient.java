49ba9257215cd2998a77dae179ed5095343930e3
JaehongDev <println@kakao.com>
2025-03-19 18:47:09 +0900

    refactor: ai client 통합 기능 추가

---
+++ artisan/artisan-infra/artisan-ai-client/src/main/java/org/artisan/ai/client/OpenAiClient.java
@@ -0,0 +1,58 @@
+package org.artisan.ai.client;
+
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.artisan.ai.property.PromptProperty;
+import org.artisan.exception.OpenAiClientException;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Profile("!stage")
+@RequiredArgsConstructor
+public class OpenAiClient implements AIClient {
+
+    private final OpenAiChatModel openAiChatModel;
+    private final PromptProperty promptProperty;
+
+    @Override
+    public FeedbackResponse execute(AiFeedbackRequest request) {
+        return subExecute(request);
+    }
+
+    private FeedbackResponse subExecute(AiFeedbackRequest request) {
+        try {
+            var beanOutputConverter = new BeanOutputConverter<>(FeedbackResponse.class);
+
+            var prompt = promptProperty.backendPromptTemplate()
+                    .create(Map.of(
+                            "format", beanOutputConverter.getFormat(),
+                            "question", request.question(),
+                            "answer", request.answer()
+                    ));
+
+            return beanOutputConverter.convert(
+                    openAiChatModel.call(prompt)
+                            .getResult()
+                            .getOutput()
+                            .getText()
+            );
+        }catch (Exception e) {
+            throw new OpenAiClientException();
+        }
+    }
+
+    public record FeedbackResponse(
+            int score,
+            String feedback,
+            String tailQuestion,
+            List<String> referenceLinks
+    ) {
+
+    }
+}
